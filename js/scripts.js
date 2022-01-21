28d679a46998dc451948eb3adaacb553639268d7
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-20 16:27:03 -0800

    add amex card validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
+let cardString = "3400000000000000";
 
-let cardString = "0000000000000000";
 
   function numberChecker(string){
     const regex = /^[0-9]+$/;
     if((string.match(regex) != null) && (string.length >= 15 && string.length <= 16)){
-      return "This card number is valid.";
+      console.log("This card number is valid.");
+      return "This card number is valid";
     }else{
-      return "This card number is not valid.";
+      console.log("This card number is not valid.");
+      return "This card number is not valid";
     }
   }
 
   numberChecker(cardString);
+
+  console.log(cardString.charAt(0));
+  console.log(cardString.charAt(1));
+
+  function isCardAmericanExpress(string){
+    const cardArray = string.split("");
+    console.log(cardArray);
+    if((parseInt(cardArray[0])=== 3) && (parseInt(cardArray[1])=== 4 || parseInt(cardArray[1]) === 7)){
+      console.log("amex");
+      console.log(cardArray[0], cardArray[1]);
+    }else{
+      console.log("invalid");
+    }
+  }
+
+  isCardAmericanExpress(cardString);
